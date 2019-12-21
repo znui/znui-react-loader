33dd5171dc71c8874c44686642342a16a1265989
yangyxu <945758912@qq.com>
2019-12-21 11:06:29 +0800

    update deps

--- index.all.js
+++
@@ -1,5 +0,0 @@
-if(!znui){
-    require('znui-react/index.all.js');
-}
-znui.react.fixCreateReactClass(require('react'), require('create-react-class'));
-module.exports = require('./index.js');
--- index.js
+++
@@ -1,2 +0,0 @@
-require('./dist/main.css');
-module.exports = require('./build/index.js');
